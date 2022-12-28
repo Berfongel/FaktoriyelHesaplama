16a915f8140e828f92be0ccad4544cbb814087d3
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 14:51:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 0 ın faktöriyeli 1
+int sayi, n;
+//faktöriyeli 0 olarak verseydim sonuç hep sıfır olurdu.
+int faktöriyel = 1;
+Console.WriteLine("Sayı değerini giriniz : ");
+sayi = Convert.ToInt32(Console.ReadLine());
+for (n = 1; n <= sayi; n++)
+{
+    //1*2*3*4*5 olması gerekiyor.Yani 1 den girilen sayıya kadar olan sayıların çarpımı faktöriyeli verir.
+    faktöriyel = faktöriyel * n;
+}
+Console.WriteLine("Faktöriyel : " + faktöriyel);
+
+
+
+
